4530036d58aadea6363ccc5b7631abbfdcb544c9
Infini24 <dywen.officiel7@gmail.com>
2025-12-08 10:28:05 +0100

    feat: Update Parizel confiserie realization details
    Refine the description for the Confiserie Parizel realization to be more general regarding event presence, moving from "marchés de Noël" to "évènements" and specifying "design" for the banner.

--- pages/RealizationsPage.tsx
+++ pages/RealizationsPage.tsx
@@ -28,7 +28,7 @@ const projects: Project[] = [
     date: 'Décembre 2024',
     link: 'https://www.facebook.com/profile.php?id=100076464574527&locale=fr_FR',
     description: `Pour la Confiserie Parizel, nous avons assuré la conception complète de leur logo original, établissant une identité de marque visuellement chaleureuse et distinctive.
-    Afin d'optimiser leur présence sur les marchés de Noël, nous avons également géré la réalisation d'une bâche publicitaire grand format (210cm sur 90cm). 
+    Afin d'optimiser leur présence sur les évènements, nous avons également géré le design d'une bâche publicitaire grand format (210cm sur 90cm). 
     Cette bâche a été conçue en garantissant une visibilité maximale et renforçant l'attractivité du point de vente durant l'événement..`,
     images: [
       {
